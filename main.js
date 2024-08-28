8442472beaa4684dfead558ac8a5c0d841a8e1e8
Martin Ricart <103967861+martinnricart@users.noreply.github.com>
2024-08-28 14:52:49 -0300

    update en-es

--- main.js
+++ main.js
@@ -1,10 +1,37 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener('DOMContentLoaded', () => {
+  const translateBtn = document.getElementById('translateBtn');
+  const elementsToTranslate = document.querySelectorAll('[data-en]');
+
+  if (translateBtn) {
+    // Define el idioma actual, predeterminado a español
+    let currentLang = 'es';
+
+    translateBtn.addEventListener('click', () => {
+        if (currentLang === 'es') {
+            // Cambiar a inglés
+            elementsToTranslate.forEach(element => {
+                element.textContent = element.getAttribute('data-en');
+            });
+            translateBtn.textContent = 'Es';
+            currentLang = 'en';
+        } else {
+            // Cambiar a español
+            elementsToTranslate.forEach(element => {
+                element.textContent = element.getAttribute('data-es');
+            });
+            translateBtn.textContent = 'En';
+            currentLang = 'es';
+        }
+    });
+  } else {
+    console.error('Botón de traducción no encontrado');
+  }
+
   const content = document.getElementById("content");
-
-  document.getElementById("trancision").style.display = "none";
-  content.classList.add("show");
-});
-
-document.querySelector('.menu-toggle').addEventListener('click', function() {
-  document.querySelector('.nav-menu').classList.toggle('active');
+  if (content) {
+    document.getElementById("trancision").style.display = "none";
+    content.classList.add("show");
+  } else {
+    console.error('Contenido no encontrado');
+  }
 });
